fc47fd2e348b3d4e7c4bac035aa09e8d7a387209
tarrantinogit <lestd3v3lop@gmail.com>
2023-02-03 13:33:57 +0200

    lesson 7 base types

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = 'John';
+age = 40;
+
+if (isBirthday) {
+    console.log(`Congrats ${userName.toUpperCase()}, age ${age + 1}`);
+}
+
